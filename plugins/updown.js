b009bf8a5104124fd8ecc080aaf82a6119cb829e
Black Amda <83172207+BlackAmda@users.noreply.github.com>
2021-06-28 20:23:07 +0530

    Delete plugins directory

--- plugins/updown.js
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
-// to-do download
