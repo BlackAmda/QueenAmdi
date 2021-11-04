15ddfc0fb25b346e37b9f6046126d871291dee32
DEVIL-ALPHA <92577442+mhmdmukrim@users.noreply.github.com>
2021-11-04 12:14:02 +0530

    Create New.js

---
+++ New.js
@@ -0,0 +1,68 @@
+{
+    "WhatsAsena":true,
+    "__ASENAJSON__":true,
+    "variables":{
+        "user":"පරිශීලක නාමය",
+        "wa_version":"පරිශීලක Whatsapp version",
+        "version":"DEVIL-ALPHA Version",
+        "os_version":"පරිශීලක පද්ධති version",
+        "device_model":"පරිශීලක Device Model",
+        "device_brand":"User Device Brand (Samsung, Nokia..)"
+    },
+    "announcements":{
+        "infoen":"```Do you have any feature requests?😈💕```\n\n*WOW!* Amazing.. Comment us your new ideas to improve and help your doings.\n\n*Visit here and comment your idea : https://chat.whatsapp.com/DxCrvmutXEPFcERUpOnSfm*\n\n```- DEVIL-ALPHA Project Team -```",
+        "infosi":"```ඔබට කිසියම් විශේෂාංග ඉල්ලීමක් තිබේද?😈💕```\n\n*WOW!* Amazing.. ඔබේ ක්‍රියාවන් සඳහා උදව් කිරීමට සහ bot වැඩිදියුණු කිරීමට ඔබේ නව අදහස් අපට Comment කරන්න.\n\n*මෙතැනට ගොස් ඔබේ අදහස කමෙන්ට් කරන්න : https://chat.whatsapp.com/DxCrvmutXEPFcERUpOnSfm*\n\n```- DEVIL-ALPHA Project Team -```",
+        "verify":"✅ Successfully verified!",
+        "road":"https://chat.whatsapp.com/DxCrvmutXEPFcERUpOnSfm"
+    },
+    "status":"200",
+    "announcement_owner":"MHMD MUKRIM",
+    "announcement_time":"GMT+3",
+    "stack":"/root/8gh32jk565/bot.js",
+    "languages":[
+        "SI",
+        "EN"
+    ],
+    "languages_count":"2",
+    "whatsasena_info":{
+        "name":"MHMD MUKRIM",
+        "version":"2.0",
+        "description":"DEVIL-ALPHA is a sinhala chat bot for WhatsApp.",
+        "legacy_repo":"https://github.com/mhmdmukrim/DEVIL-ALPHA-NEW",
+        "new_repo":"https://github.com/mhmdmukrim/DEVIL-ALPHA-NEW",
+        "logo_url":{
+            "raw":"https://i.ibb.co/8MqHBbz/IMG-20211029-WA0063.jpg",
+            "hd+":"https://i.ibb.co/8MqHBbz/IMG-20211029-WA0063.jpg",
+            "hd":"https://i.ibb.co/8MqHBbz/IMG-20211029-WA0063.jpg",
+            "sd":"https://i.ibb.co/8MqHBbz/IMG-20211029-WA0063.jpg",
+            "thumb":"https://i.ibb.co/8MqHBbz/IMG-20211029-WA0063.jpg"
+        },
+        "developers":[
+            "MHMD MUKRIM"
+        ],
+        "developers_descriptions":[
+            "Author, Base, Bug Fixes, Developer, Modules"
+        ],
+        "lisance":"GNU General Public Licence v3.0",
+        "whatsasena_desc":"DEVIL-ALPHA is a sinhala chat bot for WhatsApp.",
+        "issues":"https://github.com/mhmdmukrim/DEVIL-ALPHA-NEW/issues",
+        "pull_requests":"https://github.com/phaticusthiccy/WhatsAsenaDuplicated/pulls",
+        "wiki":"https://github.com/mhmdmukrim/DEVIL-ALPHA-NEW"
+    },
+    "details":[
+        {
+            "lang":"SI",
+            "desc":"DEVIL-ALPHA භාවිතා කරන්නන් සඳහා නිල නිවේදන.",
+            "repository":"https://github.com/mhmdmukrim/DEVIL-ALPHA-NEW",
+            "owner":"MHMD MUKRIM",
+            "owner_github":"https://github.com/mhmdmukrim"
+        },
+        {
+            "lang":"EN",
+            "desc":"Official announcements for DEVIL-ALPHA Users.",
+            "repository":"https://github.com/mhmdmukrim/DEVIL-ALPHA-NEW",
+            "owner":"MHMD MUKRIM",
+            "owner_github":"https://github.com/mhmdmukrim"
+        }
+    ]
+}
