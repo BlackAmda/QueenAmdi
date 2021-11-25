db6c244b7ecfb004d5e22b4f079c8572e99f252e
BlackAmda <sl.amdakolla@gmail.com>
2021-11-25 16:22:35 +0530

    🚮 Delete OLD files

--- QA-SQ.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    QUEEN_AMDI_OWNER: '94757405652', // Black Amda - Queen Amdi Owner
-    QUEEN_AMDI_TEAM: '94719077818,94757672873,94774976567,94766426385,94711870791', // Team - Queen Amdi TEAM
-    SEW_QUEEN_OWNER: '94785435462' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
-};
